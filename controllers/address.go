50bb0384472368fa1257fafbe08eb6baf533931a
dejaclarke <dejclark@cisco.com>
2023-09-02 11:33:03 -0700

    Set up database

---
+++ controllers/address.go
@@ -0,0 +1,27 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func AddAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func EditHomeAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func EditWorkAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func DeleteAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
--- database/database_set_up.go
+++ database/database_set_up.go
@@ -1,1 +1,35 @@
 package database
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet() *mongo.Client {
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("Failed to connect to mongodb")
+		return nil
+	}
+
+	log.Println("Successfully connected to mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+}
