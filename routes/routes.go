a7fe7de868b3bdc9d648396014ad3562b5583425
dejaclarke <dejclark@cisco.com>
2023-09-02 11:02:43 -0700

    Started filling out database package

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,14 @@
 package routes
 
 import (
+	"github.com/dejac17/Ecommerce-Project/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(incomingRooutes *gin.Engine) {
-	incomingRooutes.POST("/users/signup")
-	incomingRooutes.POST("/users/login")
-	incomingRooutes.POST("/admin/addproduct")
-	incomingRooutes.GET("/users/productreview")
-	incomingRooutes.GET("/users/search")
+	incomingRooutes.POST("/users/signup", controllers.SignUp())
+	incomingRooutes.POST("/users/login", controllers.Login())
+	incomingRooutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRooutes.GET("/users/productreview", controllers.SearchProduct())
+	incomingRooutes.GET("/users/search", controllers.SearchProductByQuery())
 }
